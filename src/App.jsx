35899a3c7f3a91bdde731264665d9a3c76d9ac20
eduardo lopez urbano <eduardolopezurbano@gmail.com>
2022-03-08 16:57:42 -0600

    ejemplos de manejo de variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+var textoVar = "texto var 2";
+let textoLet = "texto let 2";
+const textoConst = "texto conts 1";
 
-function App() {
+
+if (true) {
+textoVar = 5;
+textoLet = 10;
+
+
+}
+
+
+const App = () => {
   return (
     <div className="App">
-      <h1> Segundo Proyecto </h1>
+
+     <h1>{textoVar} </h1>
+     <h1>{textoLet} </h1>
+     <h1>{textoConst} </h1>
+
     </div>
   );
-}
+};
 
 export default App;
